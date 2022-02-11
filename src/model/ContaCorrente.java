9e4ce96990eb4f9c5b48ee1018535883698762f6
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-11 10:49:40 -0300

    Montagem de classes

---
+++ src/model/ContaCorrente.java
@@ -0,0 +1,18 @@
+package model;
+public class ContaCorrente {
+
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void sacar() {
+
+    }
+    public void depositar() {
+
+    }
+    public void transferir() {
+
+    }
+
+}
