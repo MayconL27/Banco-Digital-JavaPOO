9b98d421b18c4c5ddfa2c9abe163ed9f9a9601a9
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-22 19:19:08 -0300

    Correções caso 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 import model.PaginaInicial;
-import model.Usuario;
+
 
 public class Main {
     public static void main(String[] args) {
--- src/model/PaginaInicial.java
+++ src/model/PaginaInicial.java
@@ -6,24 +6,24 @@ import java.util.Scanner;
 public class PaginaInicial extends Usuario{
 
     public static Scanner sc = new Scanner(System.in);
-    public static Usuario usuarioLogado = null;
+    public static Usuario usuarioLogado = null; 
 
-    public static void paginaInicial() {
-        
+    public static void paginaInicial() {  
         ArrayList<Usuario> usuarios = new ArrayList();
-        
-        boolean rodando = true;
 
+        boolean rodando = true;
         while(rodando) {
         System.out.println("=== PAGINA INICIAL ===");
         System.out.println("1 Fazer cadastro");
         System.out.println("2 Fazer Login");
         System.out.println("3 Sair");
+        System.out.println("4- listar usuarios");
         System.out.println("Digite a opção: ");
-        String opcao = sc.nextLine();
+        int opcao = sc.nextInt();
+        sc.nextLine();
 
         switch (opcao) {
-            case "1":
+            case 1:
             System.out.println("*** CADASTRO ***");
             System.out.println("Digite o usuário: ");
             String user = sc.nextLine();
@@ -39,21 +39,20 @@ public class PaginaInicial extends Usuario{
                 
             break;
 
-            case "2":
+            case 2: {
             System.out.println("*** LOGIN ***");
             System.out.println("Digite o usuario: ");
             String userLog = sc.nextLine();
             System.out.println("Digite senha: ");
             String senhaLog = sc.nextLine();
 
-            boolean loginSucesso = false;
-
-            
             break;
 
-            case "3": {
+            } 
+
+            case 3: {
                 rodando = false;
-                System.out.println("Programa encerrando...");
+                System.out.println("encerrando...");
                 break;
             }
 
@@ -62,6 +61,8 @@ public class PaginaInicial extends Usuario{
             }
 
         }
+
+        System.out.println("Programa encerrado.");
         sc.close();
     } 
 }
